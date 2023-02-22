077075bd818d279bc09bece9ca05c14d114546c3
James Sheppard <jgsheppa@protonmail.com>
2023-02-22 22:41:25 +0000

    create and implement writer package from ch. 6

---
+++ cmd/writer/writer.go
@@ -0,0 +1,13 @@
+package writer
+
+import (
+	"os"
+)
+
+func WriteToFile(path string, data []byte) error {
+	err := os.WriteFile(path, data, 0600)
+	if err != nil {
+		return err
+	}
+	return os.Chmod(path, 0600)
+}
---
+++ cmd/writer/writer_test.go
@@ -0,0 +1,82 @@
+package writer_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/jgsheppa/go-tooling/cmd/writer"
+)
+
+func TestWriteToFile(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/write_test.txt"
+
+	want := []byte{1, 2, 3}
+	err := writer.WriteToFile(path, want)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stat, err := os.Stat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	perm := stat.Mode().Perm()
+	if perm != 0600 {
+		t.Errorf("want file mode 0600, got 0%o", perm)
+	}
+
+	got, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+
+}
+
+func TestWriteToFileClobbers(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/clobber_test.txt"
+	err := os.WriteFile(path, []byte{4, 5, 6}, 0600)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := []byte{1, 2, 3}
+	err = writer.WriteToFile(path, want)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := os.ReadFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Fatal(cmp.Diff(want, got))
+	}
+}
+
+func TestFilePermissions(t *testing.T) {
+	t.Parallel()
+	path := t.TempDir() + "/perms_test.txt"
+	// Pre-create empty file with open perms
+	err := os.WriteFile(path, []byte{}, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = writer.WriteToFile(path, []byte{1, 2, 3})
+	if err != nil {
+		t.Fatal(err)
+	}
+	stat, err := os.Stat(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	perm := stat.Mode().Perm()
+	if perm != 0600 {
+		t.Errorf("want file mode 0600, got 0%o", perm)
+	}
+}
