b6dde4037c744082d61ce033af866242499f50ee
jgsheppa <james.sheppard@avisio.ai>
2023-02-26 01:15:14 +0100

    create battery cmds and tests

--- cmd/battery/battery.go
+++ cmd/battery/battery.go
@@ -1,1 +1,46 @@
 package battery
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"runtime"
+	"strconv"
+)
+
+type Status struct {
+	ChargePercent int
+}
+
+var pmsetOutput = regexp.MustCompile("([0-9]+)%")
+
+func ParsePmsetOutput(data string) (Status, error) {
+	if runtime.GOOS == "darwin" {
+		panic("pmset command only functions on macOS")
+	}
+
+	matches := pmsetOutput.FindStringSubmatch(data)
+
+	if len(matches) < 2 {
+		return Status{}, fmt.Errorf("failed to parse pmset output: %q", data)
+	}
+
+	charge, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return Status{}, fmt.Errorf("failed to parse charge percentage: %q", matches[1])
+	}
+	return Status{ChargePercent: charge}, nil
+}
+
+func GetPmsetOutput() (string, error) {
+	if runtime.GOOS == "darwin" {
+		panic("pmset command only functions on macOS")
+	}
+
+	data, err := exec.Command("/usr/bin/pmset", "-g", "ps").
+		CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
--- cmd/battery/battery_test.go
+++ cmd/battery/battery_test.go
@@ -1,1 +1,46 @@
+//go:build integration
+
 package battery_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/jgsheppa/go-tooling/cmd/battery"
+)
+
+// INFO: Run this test with the following command: go test -v -tags=integration
+
+func TestParsePmset(t *testing.T) {
+	t.Parallel()
+
+	data, err := os.ReadFile("testdata/pmset.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := battery.Status{
+		ChargePercent: 100,
+	}
+
+	got, err := battery.ParsePmsetOutput(string(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestGetPmsetOutput(t *testing.T) {
+	t.Parallel()
+	text, err := battery.GetPmsetOutput()
+	if err != nil {
+		t.Fatal(err)
+	}
+	status, err := battery.ParsePmsetOutput(text)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("Charge: %d%%", status.ChargePercent)
+}
